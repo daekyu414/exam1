fb8c291c23baaf15353ae34e560ad72f96d7bd73
Daekyu Yang <eorbwogml@inu.ac.kr>
2025-12-16 16:29:32 +0900

    add calculate(), only print calculating...

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+double calculate(double a, char* op, double b)
+{
+    printf("Calculating.....\n");
+}
+
 int main()
 {
+    double a, b;
+    char op[3];
+
     printf("~~~ Welcome to my calculator ~~~\n");
+    printf("Enter expression (e.g. 1 + 2): ");
+    scanf("%lf %s %lf", &a, op, &b);
+    printf("Result = %.2f\n", calculate(a, op, b));
 
     return 0;
 }
